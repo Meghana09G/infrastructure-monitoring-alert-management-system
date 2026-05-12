5d092d45bf6ca181e560d7c479504b6e014c4fd3
Meghana09G <mxg19330@ucmo.edu>
2026-05-12 15:18:40 -0500

    Delete infrastructure-monitoring-alert-management-system directory

--- infrastructure-monitoring-alert-management-system/src/main/java/com/meghana/monitoring/repository/AlertRepository.java
+++
@@ -1,7 +0,0 @@
-package com.meghana.monitoring.repository;
-
-import com.meghana.monitoring.entity.Alert;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlertRepository extends JpaRepository<Alert, Long> {
-}
